383af14d6d762a8d08addfd53773ea651d508b1b
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2018-11-15 16:28:29 -0500

    First code

---
+++ TicTacToe/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Doruk.Ozkazanc
+ *
+ * Created on 15 Kasım 2018 Perşembe, 16:15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hey");
+    return (EXIT_SUCCESS);
+}
+
