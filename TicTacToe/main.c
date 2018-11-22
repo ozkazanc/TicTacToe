ecd50afa3a577754e5d1155bb6e29de3cbb041b7
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2018-11-21 23:01:59 -0500

    Finished Project

--- TicTacToe/main.c
+++ TicTacToe/main.c
@@ -22,46 +22,54 @@
 #define HEIGHT 4
 #define MAX_STRING_LEN 20
 int main(int argc, char** argv) {
-    char board[HEIGHT][WIDTH] = {' ','a','b','c',
-                                 'a',' ',' ',' ',
-                                 'b',' ',' ',' ', 
-                                 'c',' ',' ',' '};
-    char current_position[HEIGHT][WIDTH];
-    char player;
-    char cpu;    
-    char turn = 'x';    //x always moves first        
-    char move[MAX_STRING_LEN];   
+    char again = 'y';
+    while(again == 'y'){
+        char board[HEIGHT][WIDTH] = {' ','a','b','c',
+                                     'a',' ',' ',' ',
+                                     'b',' ',' ',' ', 
+                                     'c',' ',' ',' '};
+        char current_position[HEIGHT][WIDTH];
+        char move[MAX_STRING_LEN];
+        char player;
+        char cpu;    
+        char turn = 'x';    //x always moves first        
+        char empty;         //get rid of the '\n' in the buffer
 
-    int winner = 0;
-    int num_turns = 9;
+        int winner = 0;
+        int num_turns = 9;
 
-    printf("Choose side x or o: ");
-    scanf("%c",&player);
+        printf("Choose side x or o: ");
+        scanf("%c",&player);
+        scanf("%c",&empty);     
+        cpu = assign_cpu(player);
 
-    cpu = assign_cpu(player);
+        while(num_turns != 0 && winner == 0){                 
+            if(turn == player){
+                print_board(board); 
+                printf("Make a move: ");
+                scanf("%s",move);
+                scanf("%c",&empty);
 
-    while(num_turns != 0 && winner == 0){                 
-        if(turn == player){
-            print_board(board); 
-            printf("Make a move: ");
-            scanf("%s",move);
+                if(!make_move(board,move,turn)) continue;
+            }
+            else {
+                copy_position(current_position, board);
+                minimax(current_position, move, num_turns, cpu, player, -100, 100, 0, true);
+                if(!make_move(board,move,turn)) continue;
+            }
 
-            if(!make_move(board,move,turn)) continue;
+            turn = assign_cpu(turn);
+            winner = evaluate_position(board,player);
+            num_turns--;
         }
-        else {
-            copy_position(current_position, board);
-            minimax(current_position, move, num_turns, cpu, player, -100, 100, 0, true);
-            if(!make_move(board,move,turn)) continue;
-        }
-
-        turn = assign_cpu(turn);
-        winner = evaluate_position(board,player);
-        num_turns--;
+        print_board(board);
+        print_message(winner);
+        
+        printf("Want to play again?(y/n): ");
+        scanf("%c",&again);
+        scanf("%c",&empty);
     }
-    print_board(board);
-    print_message(winner);
-
-    //printf("Thanks for playing!\n");
+    printf("Thanks for playing!\n");
     return (EXIT_SUCCESS);
 }
 
